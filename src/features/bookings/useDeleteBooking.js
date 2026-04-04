11bf524db353f023f3d697745b64711b430cecc8
RaghadDeveloper <raghadmh620@gmail.com>
2026-04-04 11:57:59 +0300

    feat: implement booking deletion and checkout functionality

---
+++ src/features/bookings/useDeleteBooking.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+
+export function useDeleteBooking() {
+  const queryClient = useQueryClient();
+  const { mutate: deleteBooking, isLoading: isDeleting } = useMutation({
+    mutationFn: deleteBookingApi,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["bookings"] });
+      toast.success("Booking deleted successfully");
+    },
+
+    onError: () => {
+      toast.error("There was an error while deleting the booking");
+    },
+  });
+
+  return { deleteBooking, isDeleting };
+}
