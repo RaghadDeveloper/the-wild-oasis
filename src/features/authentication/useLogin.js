0eee782bded81a4da15c41963eaba7be60fa2875
RaghadDeveloper <raghadmh620@gmail.com>
2026-04-05 06:58:25 +0300

    feat: implement logout functionality and fix login bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -9,8 +9,8 @@ export function useLogin() {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: loginApi,
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
     },
 
     onError: (err) => {
