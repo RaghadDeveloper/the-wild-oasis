0908194791bad37f66de99bb2a75dd085ae7c0c5
RaghadDeveloper <raghadmh620@gmail.com>
2026-04-06 15:17:38 +0300

    feat: implement dashboard page with many features

---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,55 @@
+import { HiOutlineBriefcase, HiOutlineChartBar } from "react-icons/hi";
+import Stat from "./Stat";
+import { HiOutlineBanknotes, HiOutlineCalendarDays } from "react-icons/hi2";
+import PropTypes from "prop-types";
+import { formatCurrency } from "../../utils/helpers";
+
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  const numBookings = bookings?.length || 0;
+
+  const sales = confirmedStays?.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  const checkins = confirmedStays.length;
+
+  const occupation =
+    confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (cabinCount * numDays);
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkins}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupation * 100) + "%"}
+      />
+    </>
+  );
+}
+
+Stats.propTypes = {
+  bookings: PropTypes.array,
+  confirmedStays: PropTypes.array,
+  numDays: PropTypes.number.isRequired,
+  cabinCount: PropTypes.number.isRequired,
+};
+
+export default Stats;
